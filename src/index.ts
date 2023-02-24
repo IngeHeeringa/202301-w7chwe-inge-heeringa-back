fadbac35dee37dc910861584395b61ccd40ca124
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-24 17:58:15 +0100

    Create startServer function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-import "./loadEnvironment";
+import createDebug from "debug";
+import "./loadEnvironment.js";
+import startServer from "./server/startServer.js";
+
+const port = process.env.PORT ?? 4000;
+
+const debug = createDebug("social:root");
+
+try {
+  await startServer(+port);
+  debug(`Server listening on port ${port}`);
+} catch (error: unknown) {
+  debug((error as Error).message);
+}
