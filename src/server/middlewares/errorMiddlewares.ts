e54b24e6a5798a5e1787e07a7a2ae11a527aa5f3
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-24 18:36:53 +0100

    Create notFoundError and generalError middlewares

---
+++ src/server/middlewares/errorMiddlewares.ts
@@ -0,0 +1,31 @@
+import createDebug from "debug";
+import { type NextFunction, type Request, type Response } from "express";
+import { CustomError } from "../../CustomError.ts/CustomError.js";
+
+const debug = createDebug("social:generalErrors");
+
+export const notFoundError = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const notFoundError = new CustomError(
+    "Path not found",
+    404,
+    "Endpoint not found"
+  );
+
+  next(notFoundError);
+};
+
+export const generalError = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  debug(error.message);
+  res
+    .status(error.statusCode || 500)
+    .json({ error: error.publicMessage || "Something went wrong" });
+};
